d628b3927c10b04b3ae659779937cbfbd8d5c1fd
Andre Santiago <a2023153270@alunos.estgoh.ipc.pt>
2026-05-09 21:57:45 +0100

    yoo

--- ProjetoGrafico/src/Enums/EstadoUtilizador.java
+++ ProjetoGrafico/src/Enums/EstadoUtilizador.java
@@ -1,5 +1,17 @@
 package Enums;
 
-public enum Estadoutilizador {
-
+/**
+ * Enumeração que define os estados possíveis da conta de um utilizador no
+ * sistema.
+ *
+ * @author Hugo
+ * @version 1.0
+ */
+public enum EstadoUtilizador {
+    PENDENTE,
+    ATIVO,
+    INATIVO,
+    AGUARDA_REMOCAO,
+    REMOVIDA,
+    REJEITADO;
 }
