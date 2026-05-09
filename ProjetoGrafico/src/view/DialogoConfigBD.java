f18e890349ceeb6039d474876772e77ef6160da8
Andre Santiago <a2023153270@alunos.estgoh.ipc.pt>
2026-05-09 22:16:40 +0100

    tone

--- ProjetoGrafico/src/view/DialogoConfigBD.java
+++ ProjetoGrafico/src/view/DialogoConfigBD.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Properties;
 
