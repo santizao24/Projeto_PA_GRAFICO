f11a6e4264a0188a59709552f0eb5cf6b0850b67
Andre Santiago <a2023153270@alunos.estgoh.ipc.pt>
2026-06-27 16:37:04 +0100

    delete javadoc

--- ProjetoGrafico/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controlador"},{"l":"Enums"},{"l":"model"},{"l":"repositorio"},{"l":"util"},{"l":"view"}];updateSearchResults();
